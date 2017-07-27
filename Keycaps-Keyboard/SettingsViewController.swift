c4054f14198b0c03553fba31f70f95301dbe19de
Richard Rissanen <richardrissanen@me.com>
2017-07-27 10:24:02 -0500

    Added Settings VC and scene

---
+++ Keycaps-Keyboard/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  Keycaps
+//
+//  Created by Richard Rissanen on 7/27/17.
+//  Copyright © 2017 Screen Sized. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: {})
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
