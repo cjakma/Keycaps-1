23f91f73f7d82f26eb1e6c4e5097f566dd3170c8
Richard Rissanen <richardrissanen@me.com>
2017-07-28 11:41:45 -0500

    WIP container view and static table work

--- Keycaps-Keyboard/MoreSettingsTableViewController.swift
+++ Keycaps-Keyboard/MoreSettingsTableViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class MoreSettingsTableViewController: UITableViewController {
 
+    @IBOutlet weak var largeTextToggle:      UISwitch!
+    @IBOutlet weak var roundedCornersToggle: UISwitch!
+    @IBOutlet weak var spacebarTextToggle:   UISwitch!
+    @IBOutlet weak var largeBordersToggle:   UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,15 +33,15 @@ class MoreSettingsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 2
+//    }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 4
-    }
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return 10
+//    }
     
     @IBAction func toggleSpacebarText(_ sender: UISwitch) {
         if sender.isOn {
