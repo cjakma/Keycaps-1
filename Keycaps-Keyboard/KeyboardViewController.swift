7381ca0c022b81d6154e771688c4ed3b496ff9c1
Richard Rissanen <richardrissanen@me.com>
2017-07-31 13:40:11 -0500

    Amending scenes to account for additional height

--- Keycaps-Keyboard/KeyboardViewController.swift
+++ Keycaps-Keyboard/KeyboardViewController.swift
@@ -34,8 +34,6 @@ class KeyboardViewController: UIInputViewController {
                       "22": "#", "23": "€", "24": "£", "25": "$", "26": "¥", "27": "%", "28": "^",
                       "29": "\\", "30": "|", "31": "/", "32": "*", "33": "-", "34": "+", "35": "="]
     
-    //    let specialCharacterKeys = ["": ""]
-    
     override func updateViewConstraints() {
         super.updateViewConstraints()
         
@@ -51,20 +49,8 @@ class KeyboardViewController: UIInputViewController {
         
         self.nextKeyboardButton.addTarget(self, action: #selector(handleInputModeList(from:with:)), for: .allTouchEvents)
         
-        
-        for button in buttonCollection {
-            styleKeycap(for: button)
-            styleLegendFont(for: button)
-        }
-        
-        for button in modifierCollection { styleModifiers(for: button) }
-        
-        let allButtons: [UIButton] = buttonCollection + modifierCollection
-        for button in allButtons { styleBorders(for: button) }
-        
-        styleBackground()
-        styleSpacebar()
-        
+        styleAllButtons()
+
 
     }
     
@@ -91,6 +77,21 @@ class KeyboardViewController: UIInputViewController {
         changeCaps(buttons: buttonCollection)
         updateShift(button: shiftButton)
         
+    }
+    
+    func styleAllButtons() {
+        for button in buttonCollection {
+            styleKeycap(for: button)
+            styleLegendFont(for: button)
+        }
+        
+        for button in modifierCollection { styleModifiers(for: button) }
+        
+        let allButtons: [UIButton] = buttonCollection + modifierCollection
+        for button in allButtons { styleBorders(for: button) }
+        
+        styleBackground()
+        styleSpacebar()
     }
     
     func changeCaps(buttons:  [UIButton]!) {
